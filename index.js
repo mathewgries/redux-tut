4f626d590edfdfb3112ed8e18b453d84544e5c81
Mathew Gries <mgries@temenos.com>
2019-02-04 13:05:11 -0500

    Added reducer funtion

--- index.js
+++ index.js
@@ -37,15 +37,23 @@
     3. Never produce any side effects. (no ajax requests, do not mutate state, no interaction with the DOM (outside world))
 */
 
+// Reducer function
 function todos(state = [], action) {
-    if (action.type === 'ADD_TODO') {
-        return state.concat([action.todo])
+    switch (action.type) {
+        case 'ADD_TODO':
+            return state.concat([action.todo])
+        case 'REMOVE_TODO':
+            return state.filter((todo) => todo.id !== action.id)
+        case 'TOGGLE_TODO':
+            return state.map((todo) => todo.id !== action.id ? todo :
+                Object.assign({}, todo, { complete: !todo.complete })
+            )
+        default:
+            return state
     }
-
-    return state
 }
 
-function createStore() {
+function createStore(reducer) {
     // The store should have four parts
     // 1. The state
     // 2. Get the state
@@ -64,8 +72,28 @@ function createStore() {
         }
     }
 
+    const dispatch = (action) => {
+        // call todos
+        // loop over listeners and invoke them
+        state = reducer(state, action)
+
+        listeners.forEach((listener) => listener())
+    }
+
     return {
         getState,
         subscribe,
+        dispatch,
     }
 }
+
+const store = createStore(todos)
+
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false,
+    }
+})
